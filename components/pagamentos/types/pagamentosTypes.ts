285234d23c3ee433c80ddb65cb2a9902d82f717f
Yan Alec <yanalec448@gmail.com>
2026-01-23 15:54:42 -0300

    cleaning receitas-header

--- components/pagamentos/types/pagamentosTypes.ts
+++ components/pagamentos/types/pagamentosTypes.ts
@@ -72,3 +72,12 @@ export const FINANCEIRO_SUBCATEGORIES_MAP: Record<string, string> = {
   subsidio: "68b871d8-318c-4d83-911a-3a4a826f1870",
   valor_financiado: "972c3d02-6f0e-4d33-973a-558e6c3b8bef"
 }
+
+export const MONTHS = [
+  { value: "0", label: "Janeiro" }, { value: "1", label: "Fevereiro" },
+  { value: "2", label: "Março" }, { value: "3", label: "Abril" },
+  { value: "4", label: "Maio" }, { value: "5", label: "Junho" },
+  { value: "6", label: "Julho" }, { value: "7", label: "Agosto" },
+  { value: "8", label: "Setembro" }, { value: "9", label: "Outubro" },
+  { value: "10", label: "Novembro" }, { value: "11", label: "Dezembro" },
+]
